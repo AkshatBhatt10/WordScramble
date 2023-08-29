b289bb836a2cabb364764f467a0adb72b9076d0d
AkshatBhatt10 <143509428+AkshatBhatt10@users.noreply.github.com>
2023-08-29 18:02:40 +0530

    Add files via upload

---
+++ words.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "coding",
+        hint: "Process of making a computer program"
+    },
+    {
+        word: "charger",
+        hint: "Device used to charge devices"
+    },
+    {
+        word: "ice",
+        hint: "Solid form of water"
+    },
+    {
+        word: "emperor",
+        hint: "A noble rank for a monarch"
+    },
+    {
+        word: "intelligent",
+        hint: "Someone who is smart"
+    },
+    {
+        word: "garden",
+        hint: "Space for planting flower and plant"
+    },
+    {
+        word: "position",
+        hint: "Location of someone or something"
+    },
+    {
+        word: "football",
+        hint: "Sport played with a ball using foot"
+    },
+    {
+        word: "helmet",
+        hint: "Safety equipment used while driving"
+    },
+    {
+        word: "wrestling",
+        hint: "A legal fighting sport"
+    },
+    {
+        word: "language",
+        hint: "A principle of human communication"
+    },
+    {
+        word: "country",
+        hint: "A politically identified region"
+    },
+    {
+        word: "window",
+        hint: "A small space to look through"
+    },
+    {
+        word: "taste",
+        hint: "Ability of tongue to detect flavour"
+    },
+    {
+        word: "school",
+        hint: "A place to gain knowledge"
+    },
+    {
+        word: "aeroplane",
+        hint: "A flying transport vehicle"
+    },
+    {
+        word: "holder",
+        hint: "A device to hold on something"
+    },
+    {
+        word: "selection",
+        hint: "Process of selecting anything"
+    },
+    {
+        word: "needle",
+        hint: "A thin and sharp metal pin"
+    },
+    {
+        word: "expert",
+        hint: "Person with immense knowledge"
+    },
+    {
+        word: "clock",
+        hint: "Device to display time"
+    },
+    {
+        word: "prediction",
+        hint: "What somebody thinks will happen"
+    },
+    {
+        word: "library",
+        hint: "Place with a huge collection of books"
+    },
+]
